4e64f471b2ff187c48d563b03e3b1cc02ddb6547
wegfawefgawefg <668es218pur@gmail.com>
2021-04-04 03:38:12 -0700

    zones

---
+++ npcs.py
@@ -0,0 +1,9 @@
+from character import Character
+
+class Greeter(Character):
+    def __init__(self) -> None:
+        x, y = 2, 2
+        
+                
+        
+        super().__init__(type="grass", allows=True, char=".")
