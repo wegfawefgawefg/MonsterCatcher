b226a4ea674ddec8576ee42a750a47d733cfc8f5
wegfawefgawefg <668es218pur@gmail.com>
2021-04-05 23:57:33 -0700

    mroestuff

--- npcs.py
+++ npcs.py
@@ -1,9 +1,7 @@
 from character import Character
 from pixies import *
 
-class GreeterNPC(Character):
-    def __init__(self) -> None:
-        x, y = 2, 2
-        motion = "rotating"
-        pixie = GreeterPixie()
-        super().__init__(x, y, motion, pixie)
+
+
+
+
